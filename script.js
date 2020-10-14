cf29775440286e3547f2110dcc01f87514e37c8a
WATANAPEI <info@wpei.dev>
2020-10-14 23:58:43 +0900

    add drag and drop area

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
-function clicked() {
-    console.log("clicked!");
+let drop_area;
+
+drop_area = document.getElementById("drop-area")
+drop_area.addEventListener("dragenter", dragenter, false)
+drop_area.addEventListener("dragover", dragover, false)
+drop_area.addEventListener("drop", drop, false)
+
+function dragenter(e) {
+    e.stopPropagation()
+    e.preventDefault()
+}
+
+
+function dragover(e) {
+    e.stopPropagation()
+    e.preventDefault()
+}
+
+function drop(e) {
+    e.stopPropagation()
+    e.preventDefault()
+
+    const dt = e.dataTransfer
+    const files = dt.files
+    drop_area.append(files[0].name);
+
+
 }
