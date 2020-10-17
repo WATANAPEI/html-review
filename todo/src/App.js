bece4210487a99adfdd4bedaa1db888950156161
WATANAPEI <info@wpei.dev>
2020-10-17 23:50:57 +0900

    implement todo item adding feature

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,1 +1,22 @@
-console.log("App loaded");
+import {element} from "./view/html-util.js";
+
+export class App {
+    constructor() {
+        console.log("App initializer");
+    }
+    mount() {
+        const formElement = document.querySelector("#js-form");
+        const inputElement = document.querySelector("#js-form-input");
+        const containerElement = document.querySelector("#js-todo-list");
+        const todoItemCountElement = document.querySelector("#js-todo-count");
+        let todoItemCount = 0;
+        formElement.addEventListener("submit", () => {
+            event.preventDefault();
+            const todoItemElement = element`<li>${inputElement.value}</li>`;
+            containerElement.appendChild(todoItemElement);
+            todoItemCount += 1;
+            todoItemCountElement.textContent = `Todoアイテム数: ${todoItemCount}`;
+            inputElement.value = "";
+        })
+    }
+}
