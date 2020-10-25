276d87e1dc502f3bc1ec8d57fb2951c94df6aad5
WATANAPEI <info@wpei.dev>
2020-10-25 17:45:54 +0900

    add unit test

--- cli/src/main.js
+++ cli/src/main.js
@@ -1,6 +1,6 @@
 const program = require("commander");
 const fs = require("fs");
-const marked = require("marked");
+const md2html = require("./md2html");
 
 program.option("--gfm", "GFMを有効にする");
 program.parse(process.argv);
@@ -11,15 +11,12 @@ const cliOptions = {
     ...program.opts(),
 }
 
-
 fs.readFile(filePath, {encoding: "utf8"}, (err, data) => {
     if(err) {
         console.error(err.message);
         process.exit(1);
         return;
     }
-    const html = marked(data, {
-        gfm: cliOptions.gfm,
-    });
+    const html = md2html(data, cliOptions);
     console.log(html);
 });
