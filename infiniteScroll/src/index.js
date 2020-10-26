d7e6fcc70ab78a08392558cfeb8589f1178bc6b6
WATANAPEI <info@wpei.dev>
2020-10-26 23:29:39 +0900

    implementing infinite scroll

--- infiniteScroll/src/index.js
+++ infiniteScroll/src/index.js
@@ -1,1 +1,8 @@
-console.log("index.js: loaded.")
+document.querySelectorAll("#main").forEach(elem => {
+    elem.onscroll = function() {
+        if(this.scrollTop + this.clientHeight >= this.scrollHeight) {
+            let template = document.querySelector("#template");
+            this.appendChild(template);
+        }
+    }
+})
