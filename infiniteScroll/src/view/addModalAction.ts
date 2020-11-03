0f0add7f3a5b0b2c6c9e4553b23feb90ef9f1e86
WATANAPEI <info@wpei.dev>
2020-11-03 11:42:30 +0900

    struggling background opacity

--- infiniteScroll/src/view/addModalAction.ts
+++ infiniteScroll/src/view/addModalAction.ts
@@ -3,22 +3,22 @@ export default function addModalAction() {
     const modal = document.getElementById("modal");
     //const modalButton = document.querySelector("#modalOpen");
     const modalButton = document.getElementById("modalOpen");
+    const body = document.getElementById("body");
 
     const ms = 400;
     modal.style.transition = "opacity" + ms + "ms";
 
     modalButton.addEventListener("click", () => {
-        setTimeout(() => {
-            modal.classList.add("is_open")}
-        , 1);
-        setTimeout(() => {
-            modal.style.opacity = "1"}, 50);
+        setTimeout(() => { modal.classList.add("is_open")}, 1);
+        //setTimeout(() => { body.classList.add("opacity-half")}, 50);
+        setTimeout(() => { modal.classList.add("opacity-full")}, 50);
     });
 
     let modalClose = document.querySelectorAll(".js-modal-close");
     for(let i = 0; i < modalClose.length; i++) {
         modalClose[i].addEventListener("click", () => {
-            setTimeout(()=>{modal.style.opacity="0"}, 1);
+            setTimeout(()=>{modal.classList.remove("opacity-full")}, 1);
+            //setTimeout(() => { body.style.opacity = "1"}, 50);
             setTimeout(()=> { modal.classList.remove("is_open")}, ms);
         });
     }
